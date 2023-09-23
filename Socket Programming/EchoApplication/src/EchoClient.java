adfb6b19d5bb93fbae99e311d43358d39ba7f5a6
Safayet Rafi <safayetrafi461@gmail.com>
2023-09-23 22:27:29 +0600

    Client can send message to the server

--- Socket Programming/EchoApplication/src/EchoClient.java
+++ Socket Programming/EchoApplication/src/EchoClient.java
@@ -1,4 +1,4 @@
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
 
 public class EchoClient
@@ -7,5 +7,12 @@ public class EchoClient
      {
          System.out.println("Client started");
          Socket soc = new Socket("localhost", 9806);
+         BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+         System.out.println("Enter a string");
+         String str = userInput.readLine();
+         PrintWriter out = new PrintWriter(soc.getOutputStream(), true);
+         out.println(str);
+         BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+         System.out.println(in.readLine());
      }
 }
--- Socket Programming/EchoApplication/src/EchoServer.java
+++ Socket Programming/EchoApplication/src/EchoServer.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import  java.net.Socket;
 
@@ -9,6 +12,10 @@ public class EchoServer
          ServerSocket ss = new ServerSocket(9806);
          Socket soc = ss.accept();
          System.out.println("Connection established");
+         BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+         String str = in.readLine();
+        PrintWriter out = new PrintWriter(soc.getOutputStream(),true);
+        out.println("Server says :"+ str +" Ans. Good");
     }
 }
 
