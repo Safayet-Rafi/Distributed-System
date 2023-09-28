aa9caf824917e4ed09178877e7c4839bd2f6adae
Safayet Rafi <safayetrafi461@gmail.com>
2023-09-28 12:35:46 +0600

    Server class completed

--- Lab Task/Task-2/Group Chat/src/Server.java
+++ Lab Task/Task-2/Group Chat/src/Server.java
@@ -1,2 +1,54 @@
-public class Server {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server
+{
+    private ServerSocket serverSocket;
+
+    public Server(ServerSocket serverSocket)
+    {
+        this.serverSocket = serverSocket;
+    }
+
+    public void startServer()
+    {
+        try
+        {
+            while(!serverSocket.isClosed())
+            {
+                Socket socket = serverSocket.accept();
+                System.out.println("A new client has connected");
+
+                ClientHandler clientHandler = new ClientHandler(socket);
+                Thread thread = new Thread(clientHandler);
+            }
+        }
+        catch (IOException e)
+        {
+
+        }
+    }
+
+    public void closedSocket()
+    {
+        try
+        {
+            if(serverSocket != null)
+            {
+                serverSocket.close();
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws Exception
+    {
+         ServerSocket serverSocket = new ServerSocket(8080);
+         Server server = new Server(serverSocket);
+         server.startServer();
+    }
 }
